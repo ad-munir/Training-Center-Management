b0d97a90a2435d732dbca076d089407c035cb62e
ad-munir <mounir.ayad@etu.uae.ac.ma>
2024-01-05 16:41:57 +0100

    initial commit

---
+++ src/main/java/com/master/trainingcentermanagement/TrainingCenterManagementApplication.java
@@ -0,0 +1,13 @@
+package com.master.trainingcentermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingCenterManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingCenterManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/master/trainingcentermanagement/TrainingCenterManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.trainingcentermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingCenterManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
